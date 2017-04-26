9c604691dcc4430ef177ba75a0cdf29f2036efdd
Will <will6767@gmail.com>
2017-04-26 13:37:46 +0100

    Add commenting to chancellor class

--- core/src/com/mygdx/game/Chancellor.java
+++ core/src/com/mygdx/game/Chancellor.java
@@ -1,10 +1,18 @@
 package com.mygdx.game;
 
 import java.util.Random;
-
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
+/**
+ * This class is the Chancellor mini game. It will make the chancellor appear on a random a random tile 
+ * 
+ * @author Gandhi-Inc.
+ * @version Assessment 4
+ *      An executable version of the game can be found at: http://gandhi-inc.me/downloads/assessment4.jar
+ *      Our website is: www.gandhi-inc.me
+ *
+ */
 public class Chancellor 
 {	
 	/**
@@ -27,7 +35,7 @@ public class Chancellor
 		System.out.println("appears (chancellor class)");
 		tile.showchancellorTexture();						// Add the chancellor to the tile
 		
-		engine.getGameScreen().render(1/30);			// SHOULD RENDER THE SCREEN AGAIN?!
+		engine.getGameScreen().render(1);	// SHOULD RENDER THE SCREEN AGAIN?!
 		
 		long currentTime = System.currentTimeMillis();		// Calculate the current game time
 		long endtime = System.currentTimeMillis() + 500;	// Calculate the game time + 500ms
@@ -35,13 +43,14 @@ public class Chancellor
 		{
 			if(isHit(tile) == true)							// Checks if the tile has been hit
 			{
-				player.setMoney(player.getMoney() + 20); 	// Finds the player's money and adds 20						// Add score to player
+				player.setMoney(player.getMoney() + 20); 	// Finds the player's money and adds 20
 				// Exit the game
 			}
 			currentTime = System.currentTimeMillis();		// Updates the current game time
 		}
 		tile.hidechancellorTexture();						// Remove the chancellor from the tile
-		engine.getGameScreen().render(1);
+		engine.getGameScreen().render(1); // SHOULD RENDER THE SCREEN AGAIN?!
+		
 		//Make the game sleep for 500ms 
 		try {
 		    Thread.sleep(500);								// Sleep for 500ms
@@ -56,31 +65,32 @@ public class Chancellor
 		Random rand0 = new Random(); 						// Random to determine if chancellor will appear this round
 		Random rand1 = new Random();						// Random to determine which tile
 		
-		int random = rand0.nextInt(2);
+		int random = rand0.nextInt(2);						// Set the int called random to either 1 or 2 (50% chance)
 		
 		
-		if (random == 1)
+		if (random == 1)									// If random was 1
 		{
-			try 
+			try 											// Try - in case it throws errors
 			{
+				// Opens up a JOptionPane to inform the player that the mini game is about to take place
 				JOptionPane chancellorOptionPane = new JOptionPane("You need to catch the Chancellor! If you manage to catch him there is a prize!");
 	        	JDialog chancellorDialog = chancellorOptionPane.createDialog("Catch the Chancellor!");
-	        	chancellorDialog.setAlwaysOnTop(true);
-	        	chancellorDialog.setVisible(true);
+	        	chancellorDialog.setAlwaysOnTop(true);		// Make sure that it appears on top and is visible
+	        	chancellorDialog.setVisible(true);			// Set the dialog box to visible
 	        	
-				for(int i = 0; i < 30; i++)
+				for(int i = 0; i < 30; i++)					// Make the chancellor appear 30 times
 				{
-					int randomTile = rand1.nextInt(16);
-					Tile[] listOfTiles = engine.getTiles();
-					Tile tile = listOfTiles[randomTile];
-					System.out.println("Chancellor appears on tile " + tile.getID());
-					appears(tile, player, engine);
+					int randomTile = rand1.nextInt(16);		// Choose a random tile (between 1-16)
+					Tile[] listOfTiles = engine.getTiles();	// Define the tiles as "listOfTiles"
+					Tile tile = listOfTiles[randomTile];	// Define "tile" as a random tile amongst the list
+					System.out.println("Chancellor appears on tile " + tile.getID());	// Print which tile the chancellor appears on
+					appears(tile, player, engine); 			// Call the function appears
 				}
 				
 			} 
-			catch (InterruptedException e) 
+			catch (InterruptedException e) 					// Catch the error if any thrown
 			{
-				e.printStackTrace();
+				e.printStackTrace();						// Trace the error back up the stack
 			}
 		}
 		
