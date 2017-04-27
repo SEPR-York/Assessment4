397562f76b9dab7667d4989cf7322098f203ab4d
Steven Tomlinson <smt529@york.ac.uk>
2017-04-27 14:10:13 +0100

    Started Chancellor Tests

--- core/src/com/mygdx/game/Chancellor.java
+++ core/src/com/mygdx/game/Chancellor.java
@@ -24,7 +24,7 @@ public class Chancellor
 	}
 
 	float time = 30.0f;
-	float interval = 1.0f;
+	public static final float INTERVAL = 1.0f;
 	float currentTimeOnTile = 0.0f;
 
 	Tile currentTile = null;
@@ -41,8 +41,11 @@ public class Chancellor
 
 	private void removeFromTile()
 	{
-		currentTile.hidechancellorTexture();
-		currentTile = null;
+		if (currentTile != null)
+		{	
+			currentTile.hidechancellorTexture();
+			currentTile = null;
+		}
 	}
 
 	public void update(float dTime)
@@ -55,11 +58,11 @@ public class Chancellor
 			currentTimeOnTile += dTime;
 			time -= dTime;
 
-			if ((currentTimeOnTile > (interval / 2)) && (currentTile != null))
+			if ((currentTimeOnTile > (INTERVAL / 2)) && (currentTile != null))
 			{
 				removeFromTile();
 			}
-			else if (currentTimeOnTile > interval)
+			else if (currentTimeOnTile > INTERVAL)
 			{
 				changeTile();
 			}
@@ -72,86 +75,20 @@ public class Chancellor
 				tile.hidechancellorTexture();
 	}
 
-/*
-	
-	 * isHit looks to see if the tile is clicked while the chancellor is on the tile
-	 * @return It will return a true or false value. True if tile is hit
-	 *
-	public boolean isHit(Tile tile)
+	public float getTime()
 	{
-		// Checks to see if the tile is clicked on
-		return false;
+		return time;
+	} 
+
+	public float getCurrentTimeOnTile()
+	{
+		return currentTimeOnTile;
 	}
-	
-	/**
-	 * The Chancellor will appear on the tile selected for 500ms
-	 * @param tile
-	 * @throws InterruptedException 
-	 *
-	public void appears(Tile tile, Player player, GameEngine engine) throws InterruptedException
+
+	public Tile getCurrentTile()
 	{
-		System.out.println("appears (chancellor class)");
-		tile.showchancellorTexture();						// Add the chancellor to the tile
-		
-		engine.getGameScreen().render(1);	// SHOULD RENDER THE SCREEN AGAIN?!
-		
-		long currentTime = System.currentTimeMillis();		// Calculate the current game time
-		long endtime = System.currentTimeMillis() + 500;	// Calculate the game time + 500ms
-		while (endtime - currentTime > 0)					// While the game counts 500ms 
-		{
-			if(isHit(tile) == true)							// Checks if the tile has been hit
-			{
-				player.setMoney(player.getMoney() + 20); 	// Finds the player's money and adds 20
-				// Exit the game
-			}
-			currentTime = System.currentTimeMillis();		// Updates the current game time
-		}
-		tile.hidechancellorTexture();						// Remove the chancellor from the tile
-		engine.getGameScreen().render(1); // SHOULD RENDER THE SCREEN AGAIN?!
-		
-		//Make the game sleep for 500ms 
-		try {
-		    Thread.sleep(500);								// Sleep for 500ms
-		} catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();				// Catch the interrupted thread
-		}
-					
+		return currentTile;
 	}
-	
-	public void randomlyChooseTile(Player player, GameEngine engine)
-	{
-		Random rand0 = new Random(); 						// Random to determine if chancellor will appear this round
-		Random rand1 = new Random();						// Random to determine which tile
-		
-		int random = rand0.nextInt(2);						// Set the int called random to either 1 or 2 (50% chance)
-		
-		
-		if (random == 1)									// If random was 1
-		{
-			try 											// Try - in case it throws errors
-			{
-				// Opens up a JOptionPane to inform the player that the mini game is about to take place
-				JOptionPane chancellorOptionPane = new JOptionPane("You need to catch the Chancellor! If you manage to catch him there is a prize!");
-	        	JDialog chancellorDialog = chancellorOptionPane.createDialog("Catch the Chancellor!");
-	        	chancellorDialog.setAlwaysOnTop(true);		// Make sure that it appears on top and is visible
-	        	chancellorDialog.setVisible(true);			// Set the dialog box to visible
-	        	
-				for(int i = 0; i < 30; i++)					// Make the chancellor appear 30 times
-				{
-					int randomTile = rand1.nextInt(16);		// Choose a random tile (between 1-16)
-					Tile[] listOfTiles = engine.getTiles();	// Define the tiles as "listOfTiles"
-					Tile tile = listOfTiles[randomTile];	// Define "tile" as a random tile amongst the list
-					System.out.println("Chancellor appears on tile " + tile.getID());	// Print which tile the chancellor appears on
-					appears(tile, player, engine); 			// Call the function appears
-				}
-				
-			} 
-			catch (InterruptedException e) 					// Catch the error if any thrown
-			{
-				e.printStackTrace();						// Trace the error back up the stack
-			}
-		}
-		
-	}*/
+
 }
 
--- tests/src/com/mygdx/game/ChancellorTest.java
+++ tests/src/com/mygdx/game/ChancellorTest.java
@@ -2,20 +2,33 @@ package com.mygdx.game;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
 
-public class ChancellorTest 
+import com.badlogic.gdx.Game;
+import com.mygdx.game.GameEngine;
+
+public class ChancellorTest extends TesterFile 
 {
 
-	@Test
-	public void testIsHit() 
+    GameEngine testGameEngine;
+    private Game testGame;
+
+	@Before
+	public void setup()
 	{
-		fail("Not yet implemented");
+        testGameEngine = new GameEngine(testGame,null);
 	}
 
 	@Test
-	public void testRandomlyChooseTile() 
+	public void initialUpdate()
 	{
-		fail("Not yet implemented");
+		Chancellor chancellor = new Chancellor(testGameEngine);
+
+		chancellor.update(0);
+
+		assertTrue(chancellor.getCurrentTimeOnTile() == 0.0f);
+		assertTrue(chancellor.getCurrentTile() != null);
+
 	}
 
 }
